7556b0a7eb4e99e38fc83978752611caf2dc5ab9
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-05-21 18:19:54 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/xulybatdongbo2802/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/xulybatdongbo2802/MainActivity.java
@@ -2,13 +2,48 @@ package khoapham.ptp.phamtanphat.xulybatdongbo2802;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Button btnXulybatdongbo;
+    Thread threadA,threadB,threadC;
+    int a,b,c = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnXulybatdongbo = findViewById(R.id.buttonXulythread);
+
+        btnXulybatdongbo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                threadA = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        a = (int) Math.floor(Math.random() * 3 + 1);
+                        Log.d("BBB","Thread A : " + a);
+                    }
+                });
+                threadB = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        b = (int) Math.floor(Math.random() * 3 + 1);
+                        Log.d("BBB","Thread b : " + b);
+                    }
+                });
+                threadB = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        c = (int) Math.floor(Math.random() * 3 + 1);
+                        Log.d("BBB","Thread C : " + c);
+                    }
+                });
+
+            }
+        });
     }
 }
